7e02ae85a4f77198ce259526ccda032d2b0c67d9
jeovany.afonso <geovanisassi@gmail.com>
2024-11-29 12:13:06 +0100

    First funcionality

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ let file = document.createElement("input");
 let canvas = document.createElement("canvas");
 let video = document.createElement("video");
 
+// Adiciona o atributo tabindex para permitir foco
+canvas.setAttribute("tabindex", "0");
+
 btnUpload.addEventListener("click", function () {
     file.type = "file";
     file.accept = "video/*";
+    canvas.id = "canvasID";
     canvas.style.backgroundColor = "#ccc";
     canvas.style.border = "none";
     document.body.append(canvas);
+
+    // Garante que o canvas receba foco ao ser clicado
+    canvas.addEventListener("", () => {
+        canvas.focus();
+    });
 
     file.addEventListener("change", function () {
         if (file.files.length > 0) {
@@ -20,21 +29,19 @@ btnUpload.addEventListener("click", function () {
             // Ajustar o tamanho do canvas quando o vídeo é carregado
             video.addEventListener("loadedmetadata", function () {
                 canvas.width = (video.videoWidth < window.innerWidth) ? video.videoWidth : window.innerWidth;
-                canvas.height = (video.videoHeight < window.innerHeight) ? video.videoHeight: window.innerHeight;
-
+                canvas.height = (video.videoHeight < window.innerHeight) ? video.videoHeight : window.innerHeight;
             });
 
             video.oncanplay = () =>
                 video.play();
 
-            // Quando o vídeo começar a ser reproduzido, inicie o desenho no canvas
-            video.addEventListener("play", function () {
-                drawFrames();
-            });
+            video.addEventListener("play", () => drawFrames()
+            );
         }
     });
 
     file.click();
+    canvas.focus();
 });
 
 canvas.addEventListener("dblclick", function () {
@@ -44,10 +51,38 @@ canvas.addEventListener("dblclick", function () {
         video.pause();
 });
 
+// Evento de tecla
+canvas.addEventListener("keydown", (e) => {
+    let tecla = e.key;
+    switch (tecla) {
+        case "ArrowRight":
+            video.currentTime += 5;
+            break;
+        case "ArrowLeft":
+            video.currentTime -= 5;
+            break;
+        case "ArrowUp":
+            controlVolume(.1);
+            break;
+        case "ArrowUp":
+            controlVolume(-.1);
+            break;
+    }
+    console.log("Tecla pressionada:", tecla);
+});
+
 function drawFrames() {
     if (!video.paused && !video.ended) {
         let g2d = canvas.getContext("2d");
         g2d.drawImage(video, 0, 0, canvas.width, canvas.height); // Ajuste o tamanho para cobrir o canvas
         requestAnimationFrame(drawFrames); // Continua chamando a função para manter o desenho
+        console.log(video.volume);
     }
+}
+
+function controlVolume(num) {
+    if ((num + video.volume <= 0.0) || (num + video.volume >= 1.0))
+        video.volume = Math.min(Math.max(video.volume + num, 1.0), 0.0);
+    else
+        video.volume += num;
 }
